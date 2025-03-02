db5101d9c6e2dcdeaaff0d7df25bb0d51e341fa3
Murtaza Mister <murtazamister1@gmail.com>
2025-03-02 00:32:05 -0500

    Collection of images on the same webpage

---
+++ src/main/java/com/eulerity/hackathon/imagefinder/Collector.java
@@ -0,0 +1,8 @@
+package com.eulerity.hackathon.imagefinder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Collector {
+    public
+}
--- src/main/java/com/eulerity/hackathon/imagefinder/ImageFinder.java
+++ src/main/java/com/eulerity/hackathon/imagefinder/ImageFinder.java
@@ -10,13 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(
     name = "ImageFinder",
     urlPatterns = {"/main"}
 )
+@Slf4j
 public class ImageFinder extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
@@ -30,14 +38,51 @@ public class ImageFinder extends HttpServlet{
 			"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&format=tiny"
   	};
 
+	private boolean isValidURL(String url) {
+		try{
+			new URL(url);
+			log.info("Valid URL: " + url);
+			return true;
+		}
+		catch (Exception e) {
+			log.error("Invalid URL: " + url);
+			return false;
+		}
+	}
+
 	@Override
 	protected final void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		resp.setContentType("text/json");
-//		String path = req.getServletPath();
-//		String url = req.getParameter("url");
-//		System.out.println("Got request of:" + path + " with query param:" + url);
-//		resp.getWriter().print(GSON.toJson(testImages));
+		resp.setContentType("text/json");
+		String path = req.getServletPath();
+		String url = req.getParameter("url");
 
-		Document doc = Jsoup.connect(req.getParameter("url")).get();
+        log.info("Got request of:{} with query param: {}", path, url);
+		if(isValidURL(url)) {
+
+			Document document = Jsoup.connect(url).get();
+			Elements images = document.select("img[src]");
+			List<String> imageUrls = new ArrayList<>();
+			for(Element img : images) {
+				String imageUrl = img.attr("src");
+				if(imageUrl.startsWith("data")) continue;
+
+				if(imageUrl.startsWith("http")) {
+					log.info("URL: {}", imageUrl);
+					imageUrls.add(imageUrl);
+				}
+				else {
+					URL baseUrl = new URL(url.endsWith("/") ? url : url + "/");
+					URL finalUrl = new URL(baseUrl, imageUrl);
+					log.info("URL: {}", finalUrl);
+					imageUrls.add(finalUrl.toString());
+				}
+			}
+			resp.getWriter().print(GSON.toJson(imageUrls));
+
+		}
+		else{
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request
+			resp.getWriter().write("Invalid URL provided.");
+		}
 	}
 }
